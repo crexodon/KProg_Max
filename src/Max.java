6cdad971d537209911a2ab3aca5bce914214dbd8
crexodon <crextrex@gmail.com>
2019-04-06 15:46:52 +0200

    added fraction conversion

--- src/Max.java
+++ src/Max.java
@@ -24,21 +24,32 @@ public class Max extends JFrame implements ActionListener{
     }
 
     private void generateButtonsPanel(){
+        double negligibleRatio = 0.1;
+        String fraction;
         //make new panel
-        JPanel panel = new JPanel();
+        JPanel buttonPanel = new JPanel();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(600,600);
         this.setTitle("MAX");
-        panel.setLayout(new GridLayout( 8, 8));
+        buttonPanel.setLayout(new GridLayout( 8, 8));
 
-        //fill button array with buttons and add them to panel
+        //fill button array with buttons
+        //convert values double to a fraction, e.g. 0.3333 to 1/3
+        //add buttons to buttonPanel
         for(int i = 0; i < buttons.length; i++){
-            buttons[i] = new JButton(Double.toString(values[i]));
+            for(int j = 1;;j++){
+                double tem = values[i]/(1D/j);
+                if(Math.abs(tem - Math.round(tem)) < negligibleRatio){
+                    fraction = Math.round(tem) + "/" + i;
+                    break;
+                }
+            }
+            buttons[i] = new JButton(fraction);
             buttons[i].addActionListener(this);
-            panel.add(buttons[i]);
+            buttonPanel.add(buttons[i]);
         }
 
-        this.getContentPane().add(panel);
+        this.getContentPane().add(buttonPanel);
         this.setVisible(true);
     }
 
