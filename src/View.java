5201ef9c0ef7ff42c8bda1be2b01614fab285a1d
crexodon <crextrex@gmail.com>
2019-04-09 05:33:28 +0200

    started adding code to view from the mvc pattern

--- src/View.java
+++ src/View.java
@@ -2,8 +2,87 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class View extends JFrame{
+public class View extends JFrame implements ActionListener{
+    private JButton[] buttons = new JButton[64];
+    private String scoreW;
+    private String scoreB;
+    private String[] values = new String[64];
 
+    View(){
+        //create Frame and Panels
+        JFrame mainFrame = new JFrame("MAX");
+        JPanel mainPanel = new JPanel(new GridLayout(0, 2));
+        JPanel buttonPanel = new JPanel(new GridLayout(8,8));
+        JPanel sidePanel = new JPanel(new GridLayout(4, 0));
+
+        //set some Frame stuff
+        mainFrame.setSize(1000,600);
+        mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+
+
+        //fill button array with values and add them to buttonPanel
+        for(int i = 0; i < buttons.length; i++){
+            buttons[i] = new JButton("13/37");
+            buttons[i].addActionListener(this);
+            buttons[i].setPreferredSize(new Dimension(50,50));
+            buttonPanel.add(buttons[i]);
+        }
+
+        JLabel labelW = new JLabel(scoreW);
+        sidePanel.add(labelW);
+        JLabel labelB = new JLabel(scoreB);
+        sidePanel.add(labelB);
+
+        JButton resetButton = new JButton("RESET");
+        sidePanel.add(resetButton);
+        JButton startButton = new JButton("START");
+        sidePanel.add(startButton);
+
+        mainFrame.add(mainPanel);
+        mainPanel.add(buttonPanel);
+        mainPanel.add(sidePanel);
+
+        mainFrame.setVisible(true);
+        mainPanel.setVisible(true);
+        buttonPanel.setVisible(true);
+        sidePanel.setVisible(true);
+    }
+
+    //setter methods
+    public void setScoreW(String scoreW){
+        this.scoreW = scoreW;
+    }
+    public void setScoreB(String scoreB){
+        this.scoreB = scoreB;
+    }
+    public void setValues(String[] values){
+        this.values = values;
+    }
+    //getter methods
+    public String getScoreW(){
+        return scoreW;
+    }
+    public String getScoreB() {
+        return scoreB;
+    }
+    public String[] getValues() {
+        return values;
+    }
+
+    //refresh view for new data to draw
+    public void refresh(){
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public static void main(String[] args){
+        View view = new View();
+    }
 }
 
 /**
@@ -14,7 +93,7 @@ public class View extends JFrame{
  JPanel buttonPanel = new JPanel();
  this.setDefaultCloseOperation(EXIT_ON_CLOSE);
  this.setSize(600,600);
- this.setTitle("MAX");
+ this.setTitle("MAX");return values;
  buttonPanel.setLayout(new GridLayout( 8, 8));
 
  //fill button array with buttons
