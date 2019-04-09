d0fb0493f025af07670151785f16f8ce453d6071
crexodon <crextrex@gmail.com>
2019-04-09 04:02:06 +0200

    more refractor stuff

---
+++ src/Model.java
@@ -0,0 +1,14 @@
+public class Model {
+}
+
+/**
+ private void randomizeValue(){
+ //randomize numbers and fill the value array with values
+ for(int i = 0; i < values.length; i++){
+ double rando = Math.random() * 10;
+ double randoRound = (double) Math.round(rando * 100) / 100;
+ values[i] = randoRound;
+ System.out.println(i + ": " + values[i]);
+ }
+ }
+ **/
