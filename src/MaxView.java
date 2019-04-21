16044b15d3e5f8dd91172c1d731204eab99bb7fb
crexodon <crextrex@gmail.com>
2019-04-21 09:06:57 +0200

    started playing around with IntelliJ's GUI Designer

---
+++ src/MaxView.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MaxView {
+    private JPanel buttonPanel;
+    private JPanel contentPanel;
+    private JPanel labelPanel;
+    private JPanel menuPanel;
+    private JLabel scoreW;
+    private JLabel scoreB;
+    private JButton loadButton;
+    private JButton newButton;
+    private JButton saveButton;
+}
